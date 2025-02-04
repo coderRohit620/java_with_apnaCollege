cf719adbe6884b6bef520fb70111919b436065b1
Rohit Ravi <rohitravi62023@gmail.com>
2025-02-04 08:26:07 +0530

    learn loop

--- condi_stmnt.java
+++ condi_stmnt.java
@@ -4,16 +4,33 @@ import java.util.Scanner;
 
 public class condi_stmnt {
     public static void main(String[] args) {
-        int a,b;
+        // int a,b;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of a");
-        a = sc.nextInt();
-        System.out.println("Enter the value of b");
-        b = sc.nextInt();
-       if (a > b) {
-        System.out.println(a+" is greater than "+b);
-       } else {
-        System.out.println(b+" is greater than "+a);
-       }
+        // System.out.println("Enter the value of a");
+        // a = sc.nextInt();
+        // System.out.println("Enter the value of b");
+        // b = sc.nextInt();
+        // if (a > b) {
+        // System.out.println(a+" is greater than "+b);
+        // } else {
+        // System.out.println(b+" is greater than "+a);
+        // }
+        System.out.print("Enter the value of button :");
+        int button = sc.nextInt();
+        switch (button) {
+            case 1:
+                System.out.println("hello");
+                break;
+            case 2:
+                System.out.println("Nameste");
+                break;
+            case 3:
+                System.out.println("rohit");
+            default:
+                System.out.println("invalid input");
+                break;
+        }
+
     }
+
 }
---
+++ loop.java
@@ -0,0 +1,44 @@
+package java_with_apnaCollege;
+import java.util.*;
+
+public class loop {
+    public static void main(String[] args) {
+        // for (int i = 0; i < 11; i++) {
+        //     System.out.println(i);
+        // }
+
+// ============== While loop=========================
+        // int i = 0;
+        // while (i<11) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+// ============== Do While loop=========================
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while ( i < 11);
+
+// write a progrma sum of all natural number from 1 to 10
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number :");
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // for (int i =1 ; i < n;i++){
+        //     sum = sum + i;
+        // }
+        // System.out.println("Sum: " + sum);
+
+// print the table of input number
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number :");
+        int n = sc.nextInt();
+
+        for(int i = 1; i < 11 ; i++){
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+
+    }
+}
